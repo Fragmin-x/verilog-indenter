b2e7e0ef4f9c8cb050db2794c1ecc3c0bc8e2bb5
Flagmin <wagadao.y@outlook.com>
2020-12-13 18:17:15 +0900

    fix

--- test.v
+++ test.v
@@ -1,14 +1,14 @@
 module counter(
-  input             CLK,
-  input             RESET,
-  output reg [31:0] COUNT
+input             CLK,
+input             RESET,
+output reg [31:0] COUNT
 );
-  always @(posedge CLK) begin
-    if (RESET == 1'b1) begin
-      COUNT <= 32'd0;
-    end else begin
-      COUNT <= COUNT + 32'd1;
-      endcount
-    end
-  end
+always @(posedge CLK) begin
+if (RESET == 1'b1) begin
+COUNT <= 32'd0;
+end else begin
+COUNT <= COUNT + 32'd1;
+endcount
+end
+end
 endmodule
